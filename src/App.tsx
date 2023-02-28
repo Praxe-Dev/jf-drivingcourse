52c7cb65b79a24bcb34572b88b93228e530e29b9
Praxe Dev <46200920+Praxee@users.noreply.github.com>
2023-02-28 23:30:31 +0100

    Added calendar

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 import './App.scss'
 import Header from './components/Header'
 import Content from './components/Content'
+import Calendar from './components/Calendar'
 import Container from '@mui/material/Container'
 import Navbar from './components/Navbar'
+// import { Calendar } from 'react-calendar'
 // import Calendar from 'react-calendar';
 
 // Primary color : #085aa5
@@ -15,6 +17,7 @@ function App () {
       <Navbar />
         <Header />
         <Content />
+        <Calendar />
     </div>
   )
 }
--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import Button from '@mui/material/Button'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+// import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker'
+import dayjs, { Dayjs } from 'dayjs'
+import TextField from '@mui/material/TextField'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 let Calendar = () => {
+  const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-07'))
+
   return (
     <div>
-      <Button variant='contained'>Hello World</Button>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <StaticDatePicker
+          orientation='landscape'
+          openTo='day'
+          value={value}
+          // shouldDisableDate={isWeekend}
+          onChange={newValue => {
+            setValue(newValue)
+          }}
+          renderInput={params => <TextField {...params} />}
+        />
+      </LocalizationProvider>
     </div>
   )
 }
